00fbbded54eeebbaceaf55d4828addf4f439be67
Rafael Abreu <rafael_sabreu@hotmail.com>
2023-02-06 22:09:37 -0300

    Inline lambda expression

--- Predicate/application/Program00.java
+++ Predicate/application/Program00.java
@@ -2,7 +2,6 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 
 import entities.Produto00;
 
@@ -16,10 +15,8 @@ public class Program00 {
 		list.add(new Produto00("Mouse", 50.00));
 		list.add(new Produto00("Tablet", 350.50));
 		list.add(new Produto00("HD Case", 80.90));
-		
-		Predicate<Produto00> pred = p -> p.getPrice() >= 100;
 
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= 100);
 
 		for (Produto00 p : list) {
 			System.out.println(p);
