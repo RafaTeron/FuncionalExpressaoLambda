eed9fd4091406a798c13cacb40f48831b5db5e16
Rafael Abreu <rafael_sabreu@hotmail.com>
2023-02-12 18:43:31 -0300

    Exercicio01 Correção

--- Exercicio01/application/Program06.java
+++ Exercicio01/application/Program06.java
@@ -1,62 +1,62 @@
 package application;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import entities.Produto06;
+
+/*Fazer um programa para ler um conjunto de produtos a partir de um
+arquivo em formato .csv (suponha que exista pelo menos um produto).
+Em seguida mostrar o preço médio dos produtos. Depois, mostrar os
+nomes, em ordem decrescente, dos produtos que possuem preço
+inferior ao preço médio.*/
 
 public class Program06 {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		List<Produto06> list = new ArrayList<>();
-		
+
 		System.out.print("Insira o caminho completo do arquivo : ");
 		String path = sc.nextLine();
-		File file = new File(path);
-		
-		double soma = 0;
-		int media = 0;
-		
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-			
-			String itemCsv = br.readLine();
 
-			while (itemCsv != null) {
-				String[] fields = itemCsv.split(",");
-				String nome = fields[0];
-				double preco = Double.parseDouble(fields[1]);
-				Produto06 itens = new Produto06(nome, preco);
-				list.add(itens);				
-				
-				soma += preco;
-				media++;
-				itemCsv = br.readLine();
-								
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			List<Produto06> list = new ArrayList<>();
+
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(",");
+				list.add(new Produto06(fields[0], Double.parseDouble(fields[1])));
+				line = br.readLine();
 			}
+
+			double media = list.stream()
+					.map(p -> p.getPreco())
+					.reduce(0.0, (x, y) -> x + y) / list.size();
+
+			System.out.println("Average price: " + String.format("%.2f", media));
+
+			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+			List<String> names = list.stream()
+					.filter(p -> p.getPreco() < media)
+					.map(p -> p.getNome())
+					.sorted(comp.reversed())
+					.collect(Collectors.toList());
+
+			names.forEach(System.out::println);
 
 		} catch (IOException e) {
 			System.out.println("Error reading file: " + e.getMessage());
 		}
-        
-        soma = soma / media;
-        System.out.printf("Preço médio : %.2f%n", soma);
-        
-        list.sort((p1,p2) -> p2.getNome().toUpperCase().compareTo(p1.getNome().toUpperCase()));
-        
-        for (Produto06 p : list) {
-        	if (p.getPreco() < soma) {
-        	System.out.println(p.getNome());
-        	}
-        }
-        
+
 		sc.close();
 
 	}
